3ce5ba1a3a93735fcfdaeae767752c13886b342e
김서진 <1106laura@naver.com>
2023-12-07 20:43:55 +0900

    feat: add 90, -90 rotation

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -3,88 +3,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// public class PlayerMove : MonoBehaviour
-// {
-//     SerialPort serialPort;
-//     string portName = "/dev/cu.usbmodem142101"; // Change to your Arduino's port
-//     int baudRate = 9600;
+/*
+public class PlayerMove : MonoBehaviour
+{
+    SerialPort serialPort;
+    string portName = "/dev/cu.usbmodem142101"; // Change to your Arduino's port
+    int baudRate = 9600;
 
-//     public GameObject charModel;
-//     public float moveSpeed = 20;
-//     public float leftRightSpeed = 25;
-//     public float jump = 20;
-//     private bool isJumping = false;
-//     private bool isComingDown= false;
-//     private bool isRunning = true;
+    public GameObject charModel;
+    public float moveSpeed = 20;
+    public float leftRightSpeed = 25;
+    public float jump = 20;
+    private bool isJumping = false;
+    private bool isComingDown= false;
+    private bool isRunning = true;
 
-//     void Start() {
-//         serialPort = new SerialPort(portName, baudRate);
-//         serialPort.Open();
-//     }
+    void Start() {
+        serialPort = new SerialPort(portName, baudRate);
+        serialPort.Open();
+    }
     
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         if(isRunning) {
-//             transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
-//         }
+    // Update is called once per frame
+    void Update()
+    {
+        if(isRunning) {
+            transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+        }
 
-//         if (serialPort.IsOpen) {
-//             try {
-//                 string message = serialPort.ReadLine();
-//                 HandleMessage(message);
-//             } catch (System.Exception) { }
-//         }
+        if (serialPort.IsOpen) {
+            try {
+                string message = serialPort.ReadLine();
+                HandleMessage(message);
+            } catch (System.Exception) { }
+        }
         
-//         if(isJumping==true) {
-//             if(isComingDown==false) {
-//                 transform.Translate(Vector3.up*Time.deltaTime*jump,Space.World);
-//             } else {
-//                 transform.Translate(Vector3.down*Time.deltaTime*jump,Space.World);
-//             }
-//         }
-//     }
+        if(isJumping==true) {
+            if(isComingDown==false) {
+                transform.Translate(Vector3.up*Time.deltaTime*jump,Space.World);
+            } else {
+                transform.Translate(Vector3.down*Time.deltaTime*jump,Space.World);
+            }
+        }
+    }
 
-//     void HandleMessage(string message) {
-//         switch (message.Trim()) {
-//             case "jump":
-//                 if(!isJumping)
-//                 {
-//                     Jump();
-//                 }
-//                 break;
-//             case "stop":
-//                 if(isRunning) {
-//                     isRunning=false;
-//                     charModel.GetComponent<Animator>().Play("Idle");
-//                 }
-//                 break;
-//             case "start":
-//                 if(!isRunning) {
-//                     isRunning=true;
-//                     charModel.GetComponent<Animator>().Play("Standard Run");
-//                 }
-//                 break;
-//         }
-//     }
+    void HandleMessage(string message) {
+        switch (message.Trim()) {
+            case "jump":
+                if(!isJumping)
+                {
+                    Jump();
+                }
+                break;
+            case "stop":
+                if(isRunning) {
+                    isRunning=false;
+                    charModel.GetComponent<Animator>().Play("Idle");
+                }
+                break;
+            case "start":
+                if(!isRunning) {
+                    isRunning=true;
+                    charModel.GetComponent<Animator>().Play("Standard Run");
+                }
+                break;
+        }
+    }
 
-//     void Jump(){
-//         isJumping = true;
-//         charModel.GetComponent<Animator>().Play("Jump");
-//         StartCoroutine(JumpSequence());
-//     }
+    void Jump(){
+        isJumping = true;
+        charModel.GetComponent<Animator>().Play("Jump");
+        StartCoroutine(JumpSequence());
+    }
 
-//     IEnumerator JumpSequence() {
-//         yield return new WaitForSeconds(0.6f);
-//         isComingDown = true;
-//         yield return new WaitForSeconds(0.6f);
-//         isComingDown = false;
-//         isJumping = false;
-//         charModel.GetComponent<Animator>().Play("Standard Run");
-//     }
+    IEnumerator JumpSequence() {
+        yield return new WaitForSeconds(0.6f);
+        isComingDown = true;
+        yield return new WaitForSeconds(0.6f);
+        isComingDown = false;
+        isJumping = false;
+        charModel.GetComponent<Animator>().Play("Standard Run");
+    }
+}
+*/
 
-    
-// }
 
 public class PlayerMove : MonoBehaviour
 {
@@ -95,24 +96,26 @@ public class PlayerMove : MonoBehaviour
     private bool isJumping = false;
     private bool isComingDown= false;
 
+    private bool isRotating = false;
+    public float rotateDuration = 0.05f;
+
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (!isRotating)
         {
-            if(this.gameObject.transform.position.x > LevelBoundary.leftSide)
-            { 
-                transform.Translate(Vector3.left*leftRightSpeed*Time.deltaTime);
-            }
-        }
-        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if(this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                transform.Translate(Vector3.right*leftRightSpeed*Time.deltaTime);
+                StartCoroutine(Rotate(Quaternion.Euler(0, -90, 0)));
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                StartCoroutine(Rotate(Quaternion.Euler(0, 90, 0)));
             }
         }
+
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             if(!isJumping)
@@ -142,6 +145,23 @@ public class PlayerMove : MonoBehaviour
         isComingDown = false;
         isJumping = false;
         charModel.GetComponent<Animator>().Play("Standard Run");
+    }
+
+    IEnumerator Rotate(Quaternion deltaRotation)
+    {
+        isRotating = true;
+
+        Quaternion originalRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * deltaRotation;
+
+        for (float t = 0; t < 1; t += Time.deltaTime / rotateDuration)
+        {
+            transform.rotation = Quaternion.Lerp(originalRotation, targetRotation, t);
+            yield return null;
+        }
+
+        transform.rotation = targetRotation;
+        isRotating = false;
     }
     
 }
